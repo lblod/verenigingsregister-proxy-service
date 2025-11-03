94dd956d5fdd5e0edf6143e157be89f8495fd6ba
Boris De Vloed <code@devloed.com>
2025-11-03 15:26:37 +0100

    remove CLIENT_ID from constants

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
 export const SCOPE = process.env.SCOPE;
-export const CLIENT_ID = process.env.CLIENT_ID;
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'DEV';
 export const AUTHORIZATION_KEY = process.env.AUTHORIZATION_KEY || '';
 export const AUD = process.env.AUD;
